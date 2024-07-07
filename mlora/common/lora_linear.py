92cca2f8fc1c120559fe257bc5bf3eb5b96b3a4e
Mike Lee <mikecovlee@163.com>
2024-07-07 22:43:22 +0800

    fix lint error

--- mlora/common/lora_linear.py
+++ mlora/common/lora_linear.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from mlora.backends import MPSBackend, _backend
+from mlora.backends import _backend
 from mlora.utils import is_package_available
 
 from .modelargs import LLMModelInput, LoraConfig
